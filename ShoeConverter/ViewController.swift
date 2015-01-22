601f05f58c785226b56e5f56f311b784eb79954c
Tyler Tessmann <tyelrx@gmail.com>
2015-01-22 10:36:25 -0600

    1st commit for ShoeConverter

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPress(sender: UIButton) {
+        
+        mensSizeTextField.resignFirstResponder()
+        let conversionConstant = 30
+        let sizeFromTxtField = mensSizeTextField.text.toInt()!
+        mensSizeTextField.text = ""
+        
+        mensConvertedLabel.hidden = false
+        mensConvertedLabel.text = "\(sizeFromTxtField + conversionConstant)" + " in European Shoe Size"
+        
+        //assign user input to textfield
+        //let sizeFromTxtField = mensSizeTextField.text
+        
+        //change string to int
+        //let numFromTxtField = sizeFromTxtField.toInt()
+        //unwrap options  aka numFromTxtField
+        //var intForTxtField = numFromTxtField!
+    
+        //set constant for conversion factor & add intForTxtField to that value
+        //let conversionConstant = 30
+        //intForTxtField += conversionConstant
+        
+        //unhide converted label & then convert int back to string & finally update mensConvertedLabel text attrubute
+        //mensConvertedLabel.hidden = false
+        //let updatedSizeString = "\(intForTxtField)"
+        //mensConvertedLabel.text = updatedSizeString
+        
+        
+    }
 
 }
 
